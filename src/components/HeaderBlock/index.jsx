ab1a0872610b552183f6b813f7498dfdf8b0b8ee
Yurii Horchuk <fkca.av15gyua@gmail.com>
2019-10-18 15:15:30 +0300

    Task-#4 header

--- src/components/HeaderBlock/index.jsx
+++ src/components/HeaderBlock/index.jsx
@@ -8,7 +8,11 @@ const HeaderBlock = ({firstName, lastName}) => {
                     <img src="http://www.lakeshorecampresort.com/pictures/NewIcons/Calendar.png" alt="calendar"/>
                     <div className="header-block__logo-title">Vacation</div>
                 </div>
-                <div className="header-block__user">{firstName} {lastName}</div>
+                <div className="header-block__user">
+                    <img className="avatar" src="http://gsdl.org.in/gsdl%20image/user.png" alt=""/>
+                    <span className="full-name">{firstName} {lastName}</span>
+                </div>
+
             </div>
         </div>
     )
